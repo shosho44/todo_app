f2d32eab8e2c07296a4f0a9bad5e521779b95c8b
ssshotaro44 <shotaro2878@gmail.com>
2022-05-08 13:15:39 +0900

    localでWebページに表示できるかのテストコードを書いた

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+)
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("web/index.html")
+	t.Execute(w, nil)
+}
 func main() {
-	fmt.Println("hello, world")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":80", nil)
 }
